eb0eaea562d04cda266aa69649e252d693a8bc7d
Sabine <matisuo@gmail.com>
2018-02-15 13:20:26 +0100

    pushing full twitt site

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,29 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+
+  def tweet
+
+  	@message = tweet_params[:content]
+
+  	if Twittbot.new(@message).perform
+  		flash[:success] = 'Tweet sent ;)'
+  		redirect_to root_path
+  	else
+  		flash[:danger] = 'Something went wrong :('
+  		redirect_to root_path
+  	end
+
+  end
+
+  private 
+
+  def tweet_params
+
+  	params.require(:tweet).permit(:content)
+
+  end
+
+
+end
---
+++ app/services/twittbot.rb
@@ -0,0 +1,53 @@
+require 'dotenv'
+require 'twitter'
+
+
+class Twittbot
+
+	def initialize(message)
+
+		@message = message
+
+	end
+
+	def perform
+
+		puts "Loging in to twitter"
+
+		log_in_to_twitter
+
+		puts "send_tweet methode"
+		send_tweet
+
+		puts "end of perform"
+
+		return 1
+		
+	end
+
+
+	def log_in_to_twitter
+
+		puts "Loging in"
+
+		Dotenv.load
+
+		@client = Twitter::REST::Client.new do |config|
+  		config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+  		config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+  		config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+  		config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+		end
+
+	end
+
+
+	def send_tweet
+
+		puts "sending tweet"
+
+		@client.update(@message)
+
+	end
+
+end
